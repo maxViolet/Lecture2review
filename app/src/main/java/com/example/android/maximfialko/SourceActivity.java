046e9da740181cb62b61864ca1b3127ea704d2a8
Maxim Fialko <maxfialko@gmail.com>
2018-12-09 18:38:15 +0300

    - added "Go to source" activity

--- app/src/main/java/com/example/android/maximfialko/SourceActivity.java
+++ app/src/main/java/com/example/android/maximfialko/SourceActivity.java
@@ -3,13 +3,22 @@ package com.example.android.maximfialko;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class SourceActivity extends AppCompatActivity {
+
+    private WebView webView;
+    private static final String URL_EXTRAS = "url_extras";
+    private String url;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_source);
+
+        webView = (WebView) findViewById(R.id.web_source);
+
+        webView.loadUrl(url);
     }
 
 
